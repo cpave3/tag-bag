100e5c773afe028d5331e494eb2314e8504f9bab
Cameron Pavey <cpave3@gmail.com>
2018-08-31 17:41:48 +1000

    :recycle: Reworked FAB

--- app/components/SimpleFab.js
+++ app/components/SimpleFab.js
@@ -1,35 +1,12 @@
 import React, { Component } from 'react';
-import { TouchableHighlight, Text } from 'react-native';
+import { TouchableHighlight, Text, StyleSheet } from 'react-native';
 import { Icon } from 'react-native-elements'
-
-const styles = {
-    fab: {
-        backgroundColor: '#eb4d4b',
-        borderColor: '#eb4d4b',
-        borderWidth: 1,
-        height: 50,
-        width: 50,
-        borderRadius: 50 / 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        position: 'absolute',
-        bottom: 20,
-        right: 20,
-        shadowColor: "#000000",
-        shadowOpacity: 0.8,
-        shadowRadius: 2,
-        shadowOffset: {
-            height: 1,
-            width: 0
-        }
-    }
-};
 
 export default class SimpleFab extends Component {
     render() {
         return (
         <TouchableHighlight 
-            style={[styles.fab, { backgroundColor: this.props.color, borderColor: this.props.color }]}
+            style={[this.styles.fab, { backgroundColor: this.props.color, borderColor: this.props.color }]}
             underlayColor={this.props.color} 
             onPress={this.props.onPress}
         >
@@ -39,4 +16,27 @@ export default class SimpleFab extends Component {
         </TouchableHighlight>
         );
     }
+
+    styles = StyleSheet.create({
+        fab: {
+            backgroundColor: '#eb4d4b',
+            borderColor: '#eb4d4b',
+            borderWidth: 1,
+            height: this.props.mini ? 40 : 56,
+            width: this.props.mini ? 40 : 56,
+            borderRadius: 50,
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'absolute',
+            bottom: this.props.bottom || 20,
+            right: this.props.right || 20,
+            shadowColor: "#000000",
+            shadowOpacity: 0.8,
+            shadowRadius: 2,
+            shadowOffset: {
+                height: 1,
+                width: 0
+            }
+        }
+    });
 }
